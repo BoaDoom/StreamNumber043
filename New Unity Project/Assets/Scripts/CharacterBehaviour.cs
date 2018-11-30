eefa2db6c005d20244117faed2ac9ac392436804
BoaDoom <BoaDoom@gmail.com>
2018-11-30 15:03:05 -0800

    startingmovement

--- New Unity Project/Assets/Scripts/CharacterBehaviour.cs
+++ New Unity Project/Assets/Scripts/CharacterBehaviour.cs
@@ -5,19 +5,47 @@ using UnityEngine;
 public class CharacterBehaviour : MonoBehaviour {
 	GameObject characterCube;
 
-	// Use this for initialization
-	public Vector3 placeNum1;
-	public Vector3 placeNum2;
-	public Vector3 placeNum3;
+
+	public float timePassed;
+	public float timeAtMovementStart = 0;
+	public float timeToSpendAtLocation;
+
+	public float randomTime = 0f;
+	//public float timePassedSinceMovement;
+	public Vector2 minMaxTimeAtLocation = new Vector2 (5f, 15f);
+	private float periodOfPossibleMovement;
 
 	public Vector3 startingLocation;
 
+	public GameObject[] listOfLocationMarkers;
+
+	public Vector3 lockedStartingLocation = new Vector3(-2.71f, 6.5f, 460.5f);
+
 	void Start () {
 		startingLocation = gameObject.transform.position;
+		listOfLocationMarkers = GameObject.FindGameObjectsWithTag("positionMarker");
+		periodOfPossibleMovement = minMaxTimeAtLocation.y - minMaxTimeAtLocation.x;
+		moveToNewPosish ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		startingLocation = gameObject.transform.position;
+		if (timeToSpendAtLocation > Time.time) {
+			moveToNewPosish ();
+		}
+	}
+
+	public void moveToNewPosish(){
+		chooseNewLocation ();
+		timeAtMovementStart = Time.time;
+		randomTime = minMaxTimeAtLocation.x +(periodOfPossibleMovement * Random.value);
+		timeToSpendAtLocation = timeAtMovementStart + randomTime;
+	}
+
+	public void chooseNewLocation(){
+		int lengthOfListOfLocationMarkers = listOfLocationMarkers.Length
+		Random rnd = Random.Range (0, ((listOfLocationMarkers.Length) - 1));
+		//int r = rnd.Range  listOfLocationMarkers.Count;
+		gameObject.transform = listOfLocationMarkers[rnd].GetComponent(transform);
 	}
 }
