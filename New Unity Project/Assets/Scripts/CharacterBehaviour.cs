6c14ce6c6fd2039e2047cf27a51a4988f32bab77
BoaDoom <BoaDoom@gmail.com>
2018-11-30 13:30:26 -0800

    update unity and baby steps
    started the character script for movement around the room

---
+++ New Unity Project/Assets/Scripts/CharacterBehaviour.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterBehaviour : MonoBehaviour {
+	GameObject characterCube;
+
+	// Use this for initialization
+	public Vector3 placeNum1;
+	public Vector3 placeNum2;
+	public Vector3 placeNum3;
+
+	public Vector3 startingLocation;
+
+	void Start () {
+		startingLocation = gameObject.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		startingLocation = gameObject.transform.position;
+	}
+}
